2ca982d280c8dae8e5df9a8e16f7eee1cc818aa9
Luis Rufino Perez Romero <LuisPerez@MacBook-Air-de-Luis.local>
2017-12-02 14:24:41 -0500

    Initial Commit

---
+++ SBiztant/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SBiztant
+//
+//  Created by Luis Rufino Perez Romero on 12/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
